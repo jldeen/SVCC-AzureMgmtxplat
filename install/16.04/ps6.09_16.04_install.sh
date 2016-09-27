1646d7d005c68ec01b0d578b11afcc17305829e4
Jessica Deen <jessde@microsoft.com>
2016-09-27 16:19:00 -0700

    Typo clean up

--- install/16.04/ps6.09_16.04_install.sh
+++ install/16.04/ps6.09_16.04_install.sh
@@ -3,4 +3,4 @@
 
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.9/powershell_6.0.0-alpha.9-1ubuntu1.16.04.1_amd64.deb
 sudo apt-get install libunwind8 libicu55 -y
-sudo dpkg -i sudo dpkg -i powershell_6.0.0-alpha.9-1ubuntu1.16.04.1_amd64.deb
+sudo dpkg -i powershell_6.0.0-alpha.9-1ubuntu1.16.04.1_amd64.deb
