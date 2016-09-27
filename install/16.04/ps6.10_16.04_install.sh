f1b40b85704e2190aceb8dbdff5e38e065278a49
Jessica Deen <jessde@microsoft.com>
2016-09-27 16:14:10 -0700

    Automated .NET Core install

--- install/16.04/ps6.10_16.04_install.sh
+++ install/16.04/ps6.10_16.04_install.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotne
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update
 
-sudo apt-get install dotnet-dev-1.0.0-preview2-003131
+sudo apt-get install dotnet-dev-1.0.0-preview2-003131 -y
 
 #PowerShell Core 6.10
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.10/powershell_6.0.0-alpha.10-1ubuntu1.16.04.1_amd64.deb
