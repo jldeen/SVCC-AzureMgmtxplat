e6a58b48f51d14d308a1240356d271fea6cb5ae7
Jessica Deen <jessde@microsoft.com>
2016-09-27 16:05:19 -0700

    Organized files, isntall scripts

---
+++ install/14.04/ps6.10_14.04_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#PS 6.10 Install
+
+#PowerShell Core 6.10
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.10/powershell_6.0.0-alpha.10-1ubuntu1.14.04.1_amd64.deb
+sudo dpkg -i powershell_6.0.0-alpha.10-1ubuntu1.14.04.1_amd64.deb
+sudo apt-get install -f
