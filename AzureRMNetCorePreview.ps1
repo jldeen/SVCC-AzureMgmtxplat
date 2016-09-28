58e184f9ef9b55c428e2d6f2fc9d298369dd67c1
Jessica Deen <jessde@microsoft.com>
2016-09-27 17:05:51 -0700

    PS Script Test

---
+++ AzureRMNetCorePreview.ps1
@@ -0,0 +1,7 @@
+#AzureRM.NetCore.Preview
+
+Install-Package -Name AzureRM.NetCore.Preview -Source https://www.powershellgallery.com/api/v2 -ProviderName NuGet -ExcludeVersion -Destination ~/.local/share/powershell/Modules/
+
+Import-Module AzureRM.NetCore.Preview
+
+exit
