2b4d7d92b8ac6eec37adf83b1107d530412707ec
Jessica Deen <jessde@microsoft.com>
2016-09-27 17:24:58 -0700

    Added deployment PS scripts

---
+++ deployment/simple_linuxdeploy.ps1
@@ -0,0 +1,32 @@
+Param(
+[string]$Location,
+[string]$Resource,
+[string]$Name
+)
+
+function Check-Session () {
+    $Error.Clear()
+
+    #if context already exist
+    Get-AzureRmContext -ErrorAction Continue
+    foreach ($eacherror in $Error) {
+        if ($eacherror.Exception.ToString() -like "*Run Login-AzureRmAccount to login.*") {
+            Add-AzureRMAccount
+        }
+    }
+
+    $Error.Clear();
+}
+
+#check if session exists, if not then prompt for login
+Check-Session
+
+Write-Output "The Location you're putting your VM in is $Location."
+Write-Output "The name of your resource group is $Resource."
+Write-Output "The name of your deployment is $Name."
+
+$TempFile="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json"
+
+New-AzureRmResourceGroup $Resource $Location
+
+New-AzureRmResourceGroupDeployment -Name $Name -ResourceGroupName $Resource -TemplateUri $TempFile 
