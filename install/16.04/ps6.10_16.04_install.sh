19a9f921eadc762051297d3ecb2dfc87fbfd7e4c
Jessica Deen <jessde@microsoft.com>
2016-09-27 17:07:11 -0700

    test

--- install/16.04/ps6.10_16.04_install.sh
+++ install/16.04/ps6.10_16.04_install.sh
@@ -13,12 +13,7 @@ wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.10/
 sudo dpkg -i powershell_6.0.0-alpha.10-1ubuntu1.16.04.1_amd64.deb
 sudo apt-get install -f -y
 
-#Install Azure RM Modules
-#Address to PS Gallery https://www.powershellgallery.com/packages/AzureRM.NetCore.Preview/0.3.4
-	
-Install-Package -Name AzureRM.NetCore.Preview -Source https://www.powershellgallery.com/api/v2 -ProviderName NuGet -ExcludeVersion -Destination ~/.local/share/powershell/Modules/
-
-Import-Module AzureRM.NetCore.Preview
+powershell ../../AzureRMNetCorePreview.ps1
 
 #Install Azure CLI
 #Address https://azure.microsoft.com/en-us/documentation/articles/xplat-cli-install/
