1cea95b955499e7807767fa036bf4ee0875b2fad
Jessica Deen <jessicadeen@me.com>
2016-09-28 16:17:04 -0700

    Added PS6.10 install script for OS X 10.11

---
+++ install/osx_10.11/ps6.10_16.04_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#brew install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#.NET Core Install
+brew update
+brew install openssl
+ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
+
+wget https://go.microsoft.com/fwlink/?LinkID=827526
+	
+sudo installer -pkg dotnet-dev-osx-x64.1.0.0-preview2-003131 -target /
+
+#PowerShell Core 6.10
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.10/powershell-6.0.0-alpha.10.pkg
+sudo installer -pkg powershell-6.0.0-alpha.10.pkg -target /
+
+
+#Azure RM NetCore Preview Module Install
+powershell ../../AzureRMNetCorePreview.ps1
+
+#Install Azure CLI
+#Address https://azure.microsoft.com/en-us/documentation/articles/xplat-cli-install/
+
+brew install npm 
+brew install node.js
+sudo npm install -g azure-cli
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+#non brew install
+# wget http://aka.ms/mac-azure-cli
+
+# sudo hdiutil attach azure-cli.0.10.5.dmg
+# sudo installer -pkg /Volumes/azure-cli.0.10.5/azure-cli.0.10.5.dmg -target /
