5ebc359ec8bf4e2f890d1bd2c797876d45a6e24f
Jessica Deen <jessde@microsoft.com>
2016-11-16 11:34:30 -0500

    Updated OS X install script with exit code checks

--- install/osx_10.11/ps6.11_osx_install.sh
+++ install/osx_10.11/ps6.11_osx_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-[[ `id -u` -eq 0 ]] || { echo "Must be root to run this script, try running again with sudo"; exit 1; }
-
 #brew install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -17,34 +15,40 @@ wget https://download.microsoft.com/download/1/0/C/10C868F3-EF61-47A7-95CF-FF2AE
 	
 sudo installer -pkg dotnet-dev-osx-x64.1.0.0-preview2-003148.pkg -target /
 
-#PowerShell Core 6.10
+#PowerShell Core 6.11
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.11/powershell-6.0.0-alpha.11.pkg
 sudo installer -pkg powershell-6.0.0-alpha.11.pkg -target /
-
 
 #Azure RM NetCore Preview Module Install
 powershell Install-Module AzureRM.NetCore.Preview
 powershell Import-Module AzureRM.NetCore.Preview
+if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed PowerShell Core with AzureRM NetCore Preview Module."
+    else
+        echo "PowerShell Core with AzureRM NetCore Preview Module did not install successfully." >&2
+fi
 
 #Install Azure CLI
 #Address https://azure.microsoft.com/en-us/documentation/articles/xplat-cli-install/
 
-read -p "Do you want to install Azure CLI? " -n 1 -r
+read -p "Do you want to install Azure CLI? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     brew install npm 
     brew install node.js
     sudo npm install -g azure-cli
-    sudo ln -s /usr/bin/nodejs /usr/bin/node
+#    sudo ln -s /usr/bin/nodejs /usr/bin/node
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed Azure CLI"
+    else
+        echo "Azure CLI not installed successfully." >&2
 fi
-if [ $? -eq 0 ]
-then
-  echo "Successfully installed Azure CLI"
-else
-  echo "Azure CLI not installed successfully." >&2
+else 
+    echo "You chose not to install Azure CLI. Exiting now."
 fi
-
 
 #non brew install
 # wget http://aka.ms/mac-azure-cli
