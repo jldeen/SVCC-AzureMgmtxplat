6b9a26d66014cb7d966162becf32dfbdca9c3646
jldeen <jedeen@outlook.com>
2017-08-02 14:06:28 -0700

    Updated scripts to Beta 6.04 release

---
+++ install/14.04/betaps6.04_14.04_install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Import the public repository GPG keys
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Register the Microsoft Ubuntu repository
+curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
+# Update apt-get
+sudo apt-get update
+
+# Install PowerShell
+sudo apt-get install -y powershell
+
+#Azure RM NetCore Preview Module Install
+sudo powershell Install-Module -Name AzureRM.Netcore
+sudo powershell Import-Module AzureRM.NetCore.Preview
+
+if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed PowerShell Core with AzureRM NetCore Module."
+    else
+        echo "PowerShell Core with AzureRM NetCore Module did not install successfully." >&2
+fi
+
+#Install Azure CLI 2.0
+#Address https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
+#Github https://github.com/Azure/azure-cli/releases
+
+read -p "Do you want to install Azure CLI? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+    sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+    sudo apt-get install apt-transport-https
+    sudo apt-get update && sudo apt-get install azure-cli
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed Azure CLI 2.0"
+    else
+        echo "Azure CLI 2.0 not installed successfully" >&2
+fi
+else 
+    echo "You chose not to install Azure CLI 2.0... Exiting now."
+fi
---
+++ install/osx_10.11/betaps6.04_osx_install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#brew install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#.NET Core Install
+brew update
+brew install openssl
+mkdir -p /usr/local/lib
+ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
+
+brew install wget
+
+wget https://download.microsoft.com/download/B/9/F/B9F1AF57-C14A-4670-9973-CDF47209B5BF/dotnet-dev-osx-x64.1.0.4.pkg
+	
+sudo installer -pkg dotnet-dev-osx-x64.1.0.4.pkg -target /
+
+#PowerShell Core 6.14
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/powershell-6.0.0-beta.4-osx.10.12-x64.pkg
+sudo installer -pkg powershell-6.0.0-beta.4-osx.10.12-x64.pkg -target /
+
+#Azure RM NetCore Preview Module Install
+sudo powershell Install-Module -Name AzureRM.Netcore
+sudo powershell Import-Module AzureRM.NetCore.Preview
+if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed PowerShell Core with AzureRM NetCore Preview Module."
+    else
+        echo "PowerShell Core with AzureRM NetCore Preview Module did not install successfully." >&2
+fi
+
+#Install Azure CLI 2.0
+#Address https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
+#Github https://github.com/Azure/azure-cli/releases
+
+read -p "Do you want to install Azure CLI? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    curl -L https://aka.ms/InstallAzureCli | bash
+    exec -l $SHELL
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed Azure CLI 2.0"
+    else
+        echo "Azure CLI 2.0 not installed successfully" >&2
+fi
+else 
+    echo "You chose not to install Azure CLI 2.0... Exiting now."
+fi
